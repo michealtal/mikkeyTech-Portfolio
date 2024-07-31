d8c070e33b13c089ed8d3931eda0d154f7402494
michealtal <talabimicheal09@gmail.com>
2024-07-31 23:00:59 +0100

    fixed the side bar transition

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -1,43 +1,42 @@
-import { useState } from "react"
-import Links from "./links/Links"
-import {motion } from "framer-motion"
-import "./sidebar.scss"
-import ToggleButton from "./togglebutton/ToggleButton"
+import { useState } from "react";
+import Links from "./links/Links";
+import { motion } from "framer-motion";
+import "./sidebar.scss";
+import ToggleButton from "./togglebutton/ToggleButton";
 
 const Sidebar = () => {
-   const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-   const handleToggle = () => {
-    setOpen((prevState) => !prevState)
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
 
- };
-
-  const variants ={
-    open:{
-      clipPath:"circle (1200px at 50px 50px)",
-      transition:{
-        type:"spring",
-        stiffness:20,
-      }
+  const variants = {
+    open: {
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+        type: "spring",
+        stiffness: 20,
+      },
     },
-    closed:{
-     clipPath:"circle(30px at 50px 50px )",
-      transition:{
-        delay:0.5,
-        type:"spring",
-        stiffness:400,
-        damping:40,
+    closed: {
+      clipPath: "circle(30px at 50px 50px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
       },
     },
   };
   return (
-    <motion.div className="sidebar" animate={open ?"open" : "closed"} >
-        <motion.div className="bg" variants={variants}>
-            <Links />
-        </motion.div>
-        <ToggleButton handleToggle={handleToggle}/>
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton handleToggle={handleToggle} />
     </motion.div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
