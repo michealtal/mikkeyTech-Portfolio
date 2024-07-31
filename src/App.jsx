b8073bc27df348b3bf78fb329868d6fcb62e403b
michealtal <talabimicheal09@gmail.com>
2024-07-31 23:21:42 +0100

    fixed the side bar link  transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import Test from './Test';
-import './app.scss'
-import Contact from './component/contact/Contact';
-import Hero from './component/hero/Hero';
-import Navbar from './component/navbar/Navbar';
-import Parallax from './component/parallax/Parallax';
-import Portfolio from './component/portfolio/Portfolio';
-import Services from './component/services/Services';
-
+import Test from "./Test";
+import "./app.scss";
+import Contact from "./component/contact/Contact";
+import Hero from "./component/hero/Hero";
+import Navbar from "./component/navbar/Navbar";
+import Parallax from "./component/parallax/Parallax";
+import Portfolio from "./component/portfolio/Portfolio";
+import Services from "./component/services/Services";
 
 const App = () => {
-  return <div>
-    <section id='Homepage'>
-    <Navbar/>
-    <Hero/>
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
       </section>
-    <section id='Services'><Parallax type="services"/></section>
-    <section><Services /></section>
-    <section id='Portfolio'><Parallax type="portfolio"/></section>
-    <Portfolio />
-    <section id='Contact'>
-      <Contact />
-    </section>
-    {/* <Test/> */}
-  </div>;
+      <section>
+        <Parallax type="services" />
+      </section>
+      <section id="Services">
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* <Test/> */}
+    </div>
+  );
 };
 
 export default App;
--- src/component/sidebar/links/Links.jsx
+++ src/component/sidebar/links/Links.jsx
@@ -1,48 +1,47 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
 const variants = {
-  open:{
-   y:0,
-   opacity:1,
-
-
+  open: {
+    y: 0,
+    opacity: 1,
   },
-  closed:{
-    y:50,
-    opacity:0,
-  }
-}
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 const itemVariants = {
-  open:{
-    transition:{
-      staggeredChildren:0.1,
-    }
+  open: {
+    transition: {
+      staggeredChildren: 0.1,
+    },
   },
-  closed:{
-    transition:{
-      staggerChildren:0.05,
-      staggerDirection:-1,
-    }
-  }
-}
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
 
 const Links = () => {
-
-const items = [
-  "Homepage",
-  "Service",
-  "Portfolio",
-  "Contact",
-  "About",
- 
-]  
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
   return (
-    <motion.div className='links ' variants={variants}>
+    <motion.div className="links " variants={variants}>
       {items.map((item) => (
-       <motion.a href={`#${item}`} key={item} variants={itemVariants} whileHover={{scale:1.1}} whileTap={{scale:0.95}}>{item}</motion.a>
-    ))}</motion.div>
-  )
-}
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+};
 
-export default Links
+export default Links;
